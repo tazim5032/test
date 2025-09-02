d6cb72ae5113dd889aa90e22425ca5434d56b659
Mohammad Fakhrul Islam <mfittazim22@gmail.com>
2025-09-02 10:34:44 +0600

    html file renamed as readme

--- turndown.js
+++ turndown.js
@@ -2,7 +2,7 @@ const TurndownService = require('turndown');
 const fs = require('fs');
 
 const turndownService = new TurndownService();
-const html = fs.readFileSync('index.html', 'utf-8');
+const html = fs.readFileSync('README.html', 'utf-8');
 const markdown = turndownService.turndown(html);
 
 fs.writeFileSync('README.md', markdown);
